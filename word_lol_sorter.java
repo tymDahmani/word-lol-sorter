35c985b9d412b1793207a922fba944882d6e80c8
tay_ma_alda <temyhey@gmail.com>
2024-10-26 12:23:24 +0200

    java file is back

---
+++ word_lol_sorter.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class word_lol_sorter {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("choose the sorting method (1: alphabet, 2: reversed)");
+        String onetwo = sc.nextLine();
+        char answer = onetwo.charAt(0);
+        System.out.println("enter a list of words or letters: (seperate them with spaces)");
+        String input = sc.nextLine();
+        String[] words = input.split(" ");
+        if (answer == '1') {
+            Arrays.sort(words);
+            System.out.println(Arrays.toString(words));
+        } else if (answer == '2') {
+            Arrays.sort(words, Collections.reverseOrder());
+            System.out.println(Arrays.toString(words));
+        }
+        
+        
+    }
+}
+
